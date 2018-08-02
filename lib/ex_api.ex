17ffd010b729b094a9de545ff6c5bf6d614c83a4
EmmanuelSW <edhgt89@hotmail.com>
2018-08-01 23:40:17 -0500

    Private functions

--- lib/ex_api.ex
+++ lib/ex_api.ex
@@ -10,31 +10,31 @@ defmodule ExApi do
     |> show_message
   end
 
-  def request(url) do
+  defp request(url) do
     HTTPotion.get url
   end
 
-  def body(response) do
+  defp body(response) do
     response.body
   end
 
-  def parse_body_to_tuple(body) do
+  defp parse_body_to_tuple(body) do
     Poison.Parser.parse body
   end
 
-  def get_list_per_tuple(tuple) do
+  defp get_list_per_tuple(tuple) do
     elem(tuple, 1)
   end
 
-  def get_map_to_list(list) do
+  defp get_map_to_list(list) do
     list |> Enum.at(0)
   end
 
-  def create_message(map) do
+  defp create_message(map) do
     "> #{map["name"]} (#{map["symbol"]}) tiene un valor de: #{map["price_usd"]}"
   end
 
-  def show_message(message) do
+  defp show_message(message) do
     IO.puts message
   end
 
