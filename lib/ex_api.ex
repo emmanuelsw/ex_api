69c289e61cc40581e89cafb3030d81f51e85d749
EmmanuelSW <edhgt89@hotmail.com>
2018-08-01 23:38:37 -0500

    BTC module

--- lib/ex_api.ex
+++ lib/ex_api.ex
@@ -1,18 +1,41 @@
 defmodule ExApi do
-  @moduledoc """
-  Documentation for ExApi.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExApi.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def bitcoin do
+    request("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
+    |> body
+    |> parse_body_to_tuple
+    |> get_list_per_tuple
+    |> get_map_to_list
+    |> create_message
+    |> show_message
   end
+
+  def request(url) do
+    HTTPotion.get url
+  end
+
+  def body(response) do
+    response.body
+  end
+
+  def parse_body_to_tuple(body) do
+    Poison.Parser.parse body
+  end
+
+  def get_list_per_tuple(tuple) do
+    elem(tuple, 1)
+  end
+
+  def get_map_to_list(list) do
+    list |> Enum.at(0)
+  end
+
+  def create_message(map) do
+    "> #{map["name"]} (#{map["symbol"]}) tiene un valor de: #{map["price_usd"]}"
+  end
+
+  def show_message(message) do
+    IO.puts message
+  end
+
 end
